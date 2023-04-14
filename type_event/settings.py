c4cc5c531e7b8955e8b10d4fa55dad0ae6f407aa
Neto <fps_netodf@hotmail.com>
2023-04-14 13:12:23 -0300

    mudanças do static

--- type_event/settings.py
+++ type_event/settings.py
@@ -108,8 +108,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS =  [ BASE_DIR / 'static'] 
+STATIC_URL = '/static/'
+STATICFILES_DIRS =  [ BASE_DIR / '/static/'] 
 # STATIC_ROOT = 'static'
 
 MEDIA_ROOT = [BASE_DIR / 'media']
