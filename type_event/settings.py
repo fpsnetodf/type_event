fde49d9b500ff2be38868851f6f9987d78689258
Neto <fps_netodf@hotmail.com>
2023-04-14 13:54:34 -0300

    mudanças no static

--- type_event/settings.py
+++ type_event/settings.py
@@ -1,6 +1,8 @@
 
 
+import os
 from pathlib import Path
+
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -109,11 +111,11 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS =  [ BASE_DIR / '/static/'] 
+STATICFILES_DIRS = [BASE_DIR /'static' ]
 # STATIC_ROOT = 'static'
 
-MEDIA_ROOT = [BASE_DIR / 'media']
-MEDIA_URL = '/media/'
+MEDIA_ROOT = [ BASE_DIR / 'media']
+MEDIA_URL = 'media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
